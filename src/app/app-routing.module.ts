03e7300711b4f20a7486290289d22e29e3d5d474
BrandelleroEzequiel <ezequielbrandellero@gmail.com>
2022-12-04 14:42:33 -0300

    Authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoggedUserGuard } from './guards/logged-user.guard';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) 
+    loadChildren: ()=> import('./pages/contact/contact.module').then(m => m.ContactModule),
+    canActivate: [LoggedUserGuard]
   },
   {
-    path: 'contact',
-    loadChildren: () => import('./pages/contact/contact.module').then(m => m.ContactModule) 
-  },
-  {
-    path: '**', redirectTo: '', pathMatch: 'full'
+    path: 'login',
+    loadChildren: ()=> import('./pages/login/login.module').then(m => m.LoginModule)
   },
 ];
 
