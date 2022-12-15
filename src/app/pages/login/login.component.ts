9018492cf48212b3bbc4b8e6865ac7defc3560e3
BrandelleroEzequiel <ezequielbrandellero@gmail.com>
2022-12-15 17:25:06 -0300

    Ult Comit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { iAuthRequest } from 'src/app/interfaces/auth';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
@@ -12,10 +13,18 @@ export class LoginComponent {
 
   constructor(private auth:AuthService, private router:Router) { }
   
-    async login(form:NgForm){
-      console.log(form.value);
-      const token = await this.auth.login(form.value);
-      if(token) this.router.navigate(['']);
-    }
+    //Hecho usando NgModel
+  authData:iAuthRequest = {
+    userName : "",
+    password : ""
+  };
+
+
+  async login(form:NgForm){
+    //Valor del formulario para no usar NgModel
+    console.log(form.value);
+    const token = await this.auth.login(form.value);
+    if(token) this.router.navigate(['']);
+  }
 
 }
