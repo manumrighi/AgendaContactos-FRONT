bf0f0fb74a37da97669d63b1478ca9677a3b2fd7
manumrighi <manuelmorichi08@gmail.com>
2023-02-24 14:12:46 -0300

    ultimo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,10 +9,6 @@ const routes: Routes = [
     loadChildren: ()=> import('./pages/login/login.module').then(m => m.LoginModule),
   },
   {
-    path: 'home',
-    loadChildren: ()=> import('./pages/home/home.module').then(m => m.HomeModule),
-  },
-  {
     path: 'register',
     loadChildren: ()=> import('./pages/register/register.module').then(m => m.RegisterModule),
   },
